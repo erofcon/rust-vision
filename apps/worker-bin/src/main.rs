2e6e72c484b2b32ec1f8f932d92a33abada12a8c
erofcon <temirlan.shereuzhev@gmail.com>
2025-04-19 18:43:44 +0300

    added test run pipeline to worker-bin

--- apps/worker-bin/src/main.rs
+++ apps/worker-bin/src/main.rs
@@ -1,15 +1,32 @@
 use anyhow::Result;
-use common::config::{CommonConfig, WorkerConfig};
+use common::config::{CommonConfig, ModelConfig, WorkerConfig};
+use futures::future::join_all;
 use queue::connection::MQ;
 use queue::consumer::Consumer;
 use queue::utils::{Payload, QueueType};
 use std::error::Error;
+use std::sync::Arc;
+use storage::database::Database;
+use storage::repositories::video_repository::VideoRepository;
+use streaming::file_pipeline::FilePipeline;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    unsafe {
+        std::env::set_var("GST_DEBUG", "3");
+        std::env::set_var("RUST_BACKTRACE", "full");
+    }
+
+    gst::init()?;
+
     println!("Starting RabbitMQ workers ...");
     let common_config = CommonConfig::load()?;
     let worker_config = WorkerConfig::load()?;
+    let model_config = ModelConfig::load()?;
+
+    let db = Database::new(&common_config.database.connection_string()).await?;
+    let video_repo = Arc::new(VideoRepository::new(db.get_pool().clone()));
 
     let mq = MQ::new(&common_config.mq.connection_to_string()).await?;
 
@@ -17,29 +34,47 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     for worker_id in 0..worker_config.worker_count {
         let channel = mq.get_channel().clone();
+        let video_repo = video_repo.clone();
+        let model_config = Arc::new(model_config.clone());
 
         let mut consumer = Consumer::new(channel, QueueType::VideoProcessing).await?;
 
         consumer.set_handler(move |payload_bytes, routing_key| {
             let routing = routing_key.as_str().to_string();
             let data = payload_bytes.to_vec();
+            let video_repo = video_repo.clone();
+            let model_config = model_config.clone();
 
             async move {
                 let payload = Payload::deserialize(&data)
                     .map_err(|e| anyhow::anyhow!("Failed to deserialize payload: {}", e))?;
 
-                async_std::task::sleep(std::time::Duration::from_secs(5)).await;
-
                 println!("[Worker {}] Routing Key: {}", worker_id, routing);
-                println!("[Worker {}] Payload ID: {}", worker_id, payload.id);
+                println!(
+                    "[Worker {}] Start handling Payload ID: {}",
+                    worker_id, payload.id
+                );
 
-                // TODO: actual processing logic here
+                let video = video_repo.get_by_id(payload.id).await?.unwrap();
 
+                let stream_url = format!("rtmp://localhost/live/stream_{}", Uuid::new_v4());
+
+                let mut pipeline = FilePipeline::new(
+                    &video.file_path,
+                    model_config.input_width,
+                    model_config.input_height,
+                    &stream_url,
+                )?;
+
+                pipeline.set_frame_processor(move |_frame| Ok(()));
+
+                pipeline.start().expect("Failed to start FilePipeline");
+
+                // TODO: дальнейшая логика обработки
                 Ok(())
             }
         });
 
-        // Spawn each consumer in its own task
         let handle = tokio::spawn(async move {
             if let Err(e) = consumer.start().await {
                 eprintln!("[Worker {}] Consumer error: {}", worker_id, e);
@@ -49,8 +84,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         handles.push(handle);
     }
 
-    // Wait for all worker tasks to complete (runs indefinitely)
-    futures::future::join_all(handles).await;
+    join_all(handles).await;
 
     Ok(())
 }
